3f5b94aacc919201326beca9120576f841701506
Neosmiles <108987227+neosmiles@users.noreply.github.com>
2024-08-18 15:45:22 +0100

    update ubuntu 4

--- install_k8s_worker_ubuntu4.sh
+++ install_k8s_worker_ubuntu4.sh
@@ -6,7 +6,7 @@
 #set -euxo pipefail
 
 # Set Hostname
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && sudo hostnamectl set-hostname $(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-hostname)
+# TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && sudo hostnamectl set-hostname $(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-hostname)
 
 
 # Update package manager
